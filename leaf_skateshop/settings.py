ca28264f897c265af71697f23a4ca0f83471cee1
Chris Mecrow <mecrow@live.co.uk>
2022-07-31 18:07:05 +0000

    Created product app and initial category and product models

--- leaf_skateshop/settings.py
+++ leaf_skateshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -149,8 +150,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)	
-MEDIA_URL = '/media/'	
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' #not currently correct
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #not currently correct
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
