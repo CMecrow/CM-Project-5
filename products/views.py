8f8fd50073cfcfaa06acb245bca74f73f60e0ac5
Chris Mecrow <mecrow@live.co.uk>
2022-08-03 17:56:12 +0000

    Added beginings of site pagination

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -7,7 +8,9 @@ from .models import Product
 def all_products(request):
     """Show all products"""
 
-    products = Product.objects.all()
+    pagination = Paginator(Product.objects.all(), 9)
+    page_num = request.GET.get('page')
+    products = pagination.get_page(page_num)
 
     context = {
         'products': products,
