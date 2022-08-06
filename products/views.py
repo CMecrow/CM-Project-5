c7488736d4dce0660f112082848924fc33a4f652
Chris Mecrow <mecrow@live.co.uk>
2022-08-06 17:44:18 +0000

    Adding product_detail, current issue with url

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.core.paginator import Paginator
 
@@ -17,3 +17,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """Show an individual product"""
+
+    product = get_object_or_404(Product, sku=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
