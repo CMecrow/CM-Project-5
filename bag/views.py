46d0be2465c3a0910cf743f8d40ee1a9c0551e40
Chris Mecrow <mecrow@live.co.uk>
2022-08-13 11:54:29 +0000

    Completed functionality to adjust quantity in bag, built view and url for removing

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 # Create your models here.
 
 
@@ -27,7 +27,8 @@ def add_to_bag(request, item_id):
 
     if skate_size:
         if item_id in list(bag.keys()):
-            if skate_size in bag[item_id]['skates_by_size'].keys():
+            print(bag)
+            if skate_size in list(bag[item_id]['skates_by_size'].keys()):
                 bag[item_id]['skates_by_size'][skate_size] += quantity
             else:
                 bag[item_id]['skates_by_size'][skate_size] = quantity
@@ -51,3 +52,86 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Change the quantity of a product in the bag"""
+    bag = request.session.get('bag', {})
+
+    quantity = int(request.POST.get('quantity'))
+    skate_size = request.POST.get('bag_skate_size')
+    clothes_size = request.POST.get('bag_clothes_size')
+
+    if skate_size:
+        if quantity > 0:
+            bag[item_id]['skates_by_size'][skate_size] = quantity
+        else:
+            del bag[item_id]['skates_by_size'][skate_size]
+            if not bag[item_id]['skates_by_size']:
+                bag.pop(item_id)
+
+    elif clothes_size:
+        if quantity > 0:
+            bag[item_id]['clothes_by_size'][clothes_size] = quantity
+        else:
+            del bag[item_id]['clothes_by_size'][clothes_size]
+            if not bag[item_id]['clothes_by_size']:
+                bag.pop(item_id)
+
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove a product from the bag"""
+    try:
+        bag = request.session.get('bag', {})
+        skate_size = request.POST.get('bag_skate_size')
+        clothes_size = request.POST.get('bag_clothes_size')
+
+        if skate_size:
+            del bag[item_id]['skates_by_size'][skate_size]
+            if not bag[item_id]['skates_by_size']:
+                bag.pop(item_id)
+
+        elif clothes_size:
+            del bag[item_id]['clothes_by_size'][clothes_size]
+            if not bag[item_id]['clothes_by_size']:
+                bag.pop(item_id)
+
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+    # if 'skate_size' in request.POST:
+    #     skate_size = request.POST['skate_size']
+
+    # clothes_size = None
+
+    # if 'clothes_size' in request.POST:
+    #     clothes_size = request.POST['clothes_size']
+        
+
+    # if skate_size:
+    #     if quantity > 0:
+    #         bag[item_id]['skates_by_size'][skate_size] = quantity
+    #     else:
+    #         del bag[item_id]['skates_by_size'][skate_size]
+
+    # elif clothes_size:
+    #     if quantity > 0:
+    #         bag[item_id]['clothes_by_size'][clothes_size] = quantity
+    #     else:
+    #         del bag[item_id]['clothes_by_size'][clothes_size]
