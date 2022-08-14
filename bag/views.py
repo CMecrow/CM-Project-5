15cdd980f7e887ff8c4ae825d52cd7347ce62c92
Chris Mecrow <mecrow@live.co.uk>
2022-08-14 16:19:29 +0000

    Added further messages for actions within the bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 # Create your models here.
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a quantity of the selected product to the shopping bag"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     skate_size = None
@@ -33,23 +33,30 @@ def add_to_bag(request, item_id):
             print(bag)
             if skate_size in list(bag[item_id]['skates_by_size'].keys()):
                 bag[item_id]['skates_by_size'][skate_size] += quantity
+                messages.success(request, f'Updated {product.name} size {skate_size.upper()} quantity to {bag[item_id]["skates_by_size"][skate_size]}')
             else:
                 bag[item_id]['skates_by_size'][skate_size] = quantity
+                messages.success(request, f'Added {product.name} size {skate_size.upper()} to your bag')
         else:
             bag[item_id] = {'skates_by_size': {skate_size: quantity}}
+            messages.success(request, f'Added {product.name} size {skate_size.upper()} to your bag')
 
     elif clothes_size:
         if item_id in list(bag.keys()):
             if clothes_size in bag[item_id]['clothes_by_size'].keys():
                 bag[item_id]['clothes_by_size'][clothes_size] += quantity
+                messages.success(request, f'Updated {product.name} size {clothes_size.upper()} quantity to {bag[item_id]["clothes_by_size"][clothes_size]}')
             else:
                 bag[item_id]['clothes_by_size'][clothes_size] = quantity
+                messages.success(request, f'Added {product.name} size {clothes_size.upper()} to your bag')
         else:
             bag[item_id] = {'clothes_by_size': {clothes_size: quantity}}
+            messages.success(request, f'Added {product.name} size {clothes_size.upper()} to your bag')
 
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -62,6 +69,7 @@ def adjust_bag(request, item_id):
     """Change the quantity of a product in the bag"""
     bag = request.session.get('bag', {})
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     skate_size = request.POST.get('bag_skate_size')
     clothes_size = request.POST.get('bag_clothes_size')
@@ -69,24 +77,30 @@ def adjust_bag(request, item_id):
     if skate_size:
         if quantity > 0:
             bag[item_id]['skates_by_size'][skate_size] = quantity
+            messages.success(request, f'Updated {product.name} size {skate_size.upper()} quantity to {bag[item_id]["skates_by_size"][skate_size]}')
         else:
             del bag[item_id]['skates_by_size'][skate_size]
             if not bag[item_id]['skates_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed {product.name} size {skate_size.upper()} from your bag')
 
     elif clothes_size:
         if quantity > 0:
             bag[item_id]['clothes_by_size'][clothes_size] = quantity
+            messages.success(request, f'Updated {product.name} size {clothes_size.upper()} quantity to {bag[item_id]["clothes_by_size"][clothes_size]}')
         else:
             del bag[item_id]['clothes_by_size'][clothes_size]
             if not bag[item_id]['clothes_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed {product.name} size {clothes_size.upper()} from your bag')
 
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
     
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -94,6 +108,7 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove a product from the bag"""
+    product = get_object_or_404(Product, pk=item_id)
     try:
         bag = request.session.get('bag', {})
         skate_size = request.POST.get('bag_skate_size')
@@ -103,39 +118,21 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['skates_by_size'][skate_size]
             if not bag[item_id]['skates_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed {product.name} size {skate_size.upper()} from your bag')
 
         elif clothes_size:
             del bag[item_id]['clothes_by_size'][clothes_size]
             if not bag[item_id]['clothes_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed {product.name} size {clothes_size.upper()} from your bag')
 
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
-
-
-    # if 'skate_size' in request.POST:
-    #     skate_size = request.POST['skate_size']
-
-    # clothes_size = None
-
-    # if 'clothes_size' in request.POST:
-    #     clothes_size = request.POST['clothes_size']
-        
-
-    # if skate_size:
-    #     if quantity > 0:
-    #         bag[item_id]['skates_by_size'][skate_size] = quantity
-    #     else:
-    #         del bag[item_id]['skates_by_size'][skate_size]
-
-    # elif clothes_size:
-    #     if quantity > 0:
-    #         bag[item_id]['clothes_by_size'][clothes_size] = quantity
-    #     else:
-    #         del bag[item_id]['clothes_by_size'][clothes_size]
