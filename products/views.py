dbb35413f13fa1acb694e5e2c71e5033523daf9b
Chris Mecrow <mecrow@live.co.uk>
2022-08-27 11:20:26 +0000

    Finished add, edit delete products, redesign nav

--- products/views.py
+++ products/views.py
@@ -140,9 +140,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product to the store')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product to the store, please check form')
     else:
@@ -155,3 +155,35 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """Edit an existing product"""
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated {product.name}')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Failed to update {product.name}, please check details')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'Currently editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """Delete an existing product"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Successfully deleted {product.name}')
+    return redirect(reverse('products'))
